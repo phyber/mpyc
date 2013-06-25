4bb79532b6c506e5ec0558a79e0df1d7f229ddd1
phyber <phyber@localhost>
2013-06-25 10:20:21 +0100

    Local config now in local.cfg in app root directory.

--- mpyc/__init__.py
+++ mpyc/__init__.py
@@ -6,7 +6,7 @@ import flask
 app = flask.Flask(__name__)
 # Load defaults.
 app.config.from_object('mpyc.default_settings')
-# Load user config.
-app.config.from_pyfile(os.path.expanduser('~/.mpyc.conf'))
+# Load user config, don't complain if it's missing.
+app.config.from_pyfile('../local.cfg', silent=True)
 
 import mpyc.views
--- mpyc/default_settings.py
+++ mpyc/default_settings.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 
+# Running with debugging on will automatically reload Flask when files are
+# changed.
 DEBUG = False
 
+# IP address and port that the app server should listen on.
 HOST = '0.0.0.0'
 PORT = None
 
+# MPD connection details.
 MPD_HOST = '127.0.0.1'
 MPD_PORT = 6000
 MPD_PASS = None
