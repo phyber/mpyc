6571dcd4665c183284c7f280ffef9cdf1dad3867
phyber <phyber@localhost>
2013-06-27 16:56:29 +0100

    Add docstrings to views.py

--- mpyc/views.py
+++ mpyc/views.py
@@ -12,7 +12,12 @@ JSON_SEPARATORS = (',', ':')
 MPC = mpyc.mpdclient.MPC(app.config)
 
 def jsonify(arg):
-	# Provide an empty dict to jsonify if the command doesn't produce output.
+	"""
+	Produces JSON output based on the given argument.
+	If the argument is None (command producing the arg didn't
+	produce output, create an empty dict for returning to the frontend.
+	Returns a flask.Response() to the caller.
+	"""
 	if arg is None:
 		arg = {}
 	status_code = 200
@@ -26,14 +31,25 @@ def jsonify(arg):
 
 @app.route("/", methods=['GET'])
 def index_view():
+	"""
+	Redirects to the /mpd/
+	"""
 	return flask.redirect(flask.url_for('mpd_view'))
 
 @app.route("/mpd/", methods=['GET'])
 def mpd_view():
+	"""
+	Renders the mpd.html template.
+	"""
 	return flask.render_template("mpd.html")
 
 @app.route("/mpd/<mpd_command>.json", methods=['GET'])
 def mpd_command(mpd_command):
+	"""
+	Handles /mpd/<mpd_command>.json GET requests.
+	Commands are passed to MPC.execute, the results are jsonified
+	and returned.
+	"""
 	try:
 		data = MPC.execute(mpd_command)
 	except InvalidCommand as e:
