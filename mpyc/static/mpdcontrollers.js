1ca2bd1a88aebb6374d4316863fc881994653898
phyber <phyber@localhost>
2013-07-26 14:28:44 +0100

    Reformat angular.module a bit

--- mpyc/static/mpdcontrollers.js
+++ mpyc/static/mpdcontrollers.js
@@ -12,79 +12,78 @@ var DURATION_LONG_STR = ['d ', 'h ', 'm ', 's'];
 var DURATION_SHORT_STR = [':', ':', ':', ''];
 var PLAYLIST_PAGE_SIZE = 50;
 
-angular.module('mpdFilters', []).
-	filter('duration', function() {
-		return function(secs, longFormat) {
-			if (secs == undefined) {
-				return '';
-			}
-			var str = '';
-			for (var i in DURATION_SECS) {
-				var show = false;
-				var num = parseInt(secs / DURATION_SECS[i]);
+var app = angular.module('mpd', []);
+app.filter('duration', function() {
+	return function(secs, longFormat) {
+		if (secs == undefined) {
+			return '';
+		}
+		var str = '';
+		for (var i in DURATION_SECS) {
+			var show = false;
+			var num = parseInt(secs / DURATION_SECS[i]);
 
-				secs -= num * DURATION_SECS[i];
+			secs -= num * DURATION_SECS[i];
 
-				if (longFormat) {
-					var show = (num != 0);
-					str += show ? num + DURATION_LONG_STR[i] : '';
-				}
-				else {
-					// Pad with a leading zero if it's no the 'days' value
-					if (i > 0 && num < 10) {
-						num = '0' + num;
-					}
-					// Show this number if > 0 and force it to show
-					// if it's a minutes or a seconds number.
-					var show = (num != 0 || i > 1)
-						str += show ? num + DURATION_SHORT_STR[i] : '';
-				}
-			}
-			return str;
-		}
-	}).
-	filter('paging', function() {
-		return function(input, startFrom) {
-			/* Sometimes input is undefined for some reason */
-			if (input == undefined) {
-				return;
-			}
-			startFrom = +startFrom;
-			return input.slice(startFrom);
-		}
-	}).
-	filter('state', function() {
-		var MPD_STATES = {
-			'pause': 'paused',
-			'play': 'playing',
-			'stop': 'stopped',
-		};
-		return function(state) {
-			return MPD_STATES[state];
-		}
-	}).
-	filter('volume', function() {
-		return function(volume) {
-			if (volume == -1) {
-				return "N/A";
+			if (longFormat) {
+				var show = (num != 0);
+				str += show ? num + DURATION_LONG_STR[i] : '';
 			}
 			else {
-				return volume;
+				// Pad with a leading zero if it's no the 'days' value
+				if (i > 0 && num < 10) {
+					num = '0' + num;
+				}
+				// Show this number if > 0 and force it to show
+				// if it's a minutes or a seconds number.
+				var show = (num != 0 || i > 1)
+					str += show ? num + DURATION_SHORT_STR[i] : '';
 			}
 		}
-	}).
-	service('cache', function() {
-		var values = {};
-		return {
-			get: function(key) {
-				return values[key];
-			},
-			set: function(key, value) {
-				values[key] = value;
-			},
-		};
-	})
-;
+		return str;
+	}
+});
+app.filter('paging', function() {
+	return function(input, startFrom) {
+		/* Sometimes input is undefined for some reason */
+		if (input == undefined) {
+			return;
+		}
+		startFrom = +startFrom;
+		return input.slice(startFrom);
+	}
+});
+app.filter('state', function() {
+	var MPD_STATES = {
+		'pause': 'paused',
+		'play': 'playing',
+		'stop': 'stopped',
+	};
+	return function(state) {
+		return MPD_STATES[state];
+	}
+});
+app.filter('volume', function() {
+	return function(volume) {
+		if (volume == -1) {
+			return "N/A";
+		}
+		else {
+			return volume;
+		}
+	}
+});
+app.service('cache', function() {
+	var values = {};
+	return {
+		get: function(key) {
+			return values[key];
+		},
+		set: function(key, value) {
+			values[key] = value;
+		},
+	};
+});
 
 function PlaylistInfoCtrl($rootScope, $scope, $http, $filter) {
 	$scope.headers = [
@@ -110,7 +109,6 @@ function PlaylistInfoCtrl($rootScope, $scope, $http, $filter) {
 	$rootScope.$on('currentsongpage', function(event, currentsongpage) {
 		$scope.currentSongPage = currentsongpage;
 		$scope.currentPage = currentsongpage;
-		console.log("Got currentsongpage");
 	});
 	$http.get('/mpd/playlistinfo.json').success(function(data) {
 		var total_time_secs = 0;
@@ -118,6 +116,7 @@ function PlaylistInfoCtrl($rootScope, $scope, $http, $filter) {
 			var current = data[i];
 			var track_time = current['time'];
 			total_time_secs += parseInt(track_time);
+			// Replaces the time in seconds with the human readable time as rendered by the duration filter.
 			current['time'] = $filter('duration')(track_time);
 		}
 		$scope.playlist_total_time_secs = total_time_secs;
