a2969e1c766f7232459fc10ecb92cecc2c76220a
phyber <phyber@localhost>
2013-08-01 11:58:41 +0100

    Move app.run() into a __name__ == '__main__' block.

--- runserver.py
+++ runserver.py
@@ -3,8 +3,9 @@
 from mpyc import app
 
 # Run application
-app.run(
-    host=app.config.get('SERVER_HOST', None),
-    port=app.config.get('SERVER_PORT', None),
-    threaded=True
-)
+if __name__ == '__main__':
+    app.run(
+        host=app.config.get('SERVER_HOST', None),
+        port=app.config.get('SERVER_PORT', None),
+        threaded=True
+    )
